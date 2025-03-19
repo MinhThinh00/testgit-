b35200ccb4a4fba7326b7d0ec7dbc51e56e95777
thinhm <minhthinh3002@gmail.com>
2025-03-19 13:56:17 +0700

    ok

--- ok.js
+++ ok.js
@@ -1,4 +1,9 @@
 const greet = (name = "branch1") => {
     console.log(`Hello ${name}`);
   };
+
+  const onen = (name = "b4567890") => {
+    console.log(`Hello ${name}`);
+  };
+  onen();
   greet();
